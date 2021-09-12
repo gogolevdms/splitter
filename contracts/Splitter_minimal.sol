5b934c63b5e3bf2dd7f031430f31247774059ab8
Dmitriy Gogolev <gogolevdms@gmail.com>
2021-09-12 23:54:26 +0300

    remove total share variable

--- contracts/Splitter_minimal.sol
+++ contracts/Splitter_minimal.sol
@@ -4,20 +4,18 @@ pragma solidity ^0.8.7;
 contract Splitter {
     uint public relayerShare;
     uint public payeeShare;
-    uint public totalShares;
 
     address public payee;
 
     constructor(uint relayerShare_, address payee_, uint payeeShare_) payable {
         relayerShare = relayerShare_;
         payeeShare = payeeShare_;
-        totalShares = relayerShare_ + payeeShare_;
 
         payee = payee_;
     }
 
     function forward(address payable relayer) external payable {
-        uint payment = msg.value * relayerShare / totalShares;
+        uint payment = msg.value * relayerShare / (relayerShare + payeeShare);
         (bool success, ) = relayer.call{value: payment}("");
         require(success, "Address: unable to send value, relayer may have reverted");
 
